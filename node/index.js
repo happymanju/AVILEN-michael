68a25248dfa85269300fb68601d8ab84e5c491a3
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2021-03-15 12:56:32 +0900

    Added FizzBuzz; TODO: parse XML from frontend

--- node/index.js
+++ node/index.js
@@ -1,17 +1,49 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
    const endpoint = req.url;
-   if( endpoint==='/start' ){
-      fs.readFile('./index.html',(err, data)=>{
-         res.writeHead(200, {'Content-Type': 'text/html'});
+   if (endpoint === '/start') {
+      fs.readFile('./index.html', (err, data) => {
+         res.writeHead(200, { 'Content-Type': 'text/html' });
          res.write(data);
          res.end();
       });
    }
-   if( endpoint==='/api' ){
+   if (endpoint === '/api') {
       // ここに処理を記述してください。  
+      //HTMLのフロントの方からボタンを押すと'/api'のendpointをアクセスし、ここにあるコードが発動
+
+      //FizzBuzzのアルゴリズムの基本はできた
+      function fizzBuzz(pattern, callback) {
+         const js = JSON.parse(pattern);
+         const result = { data: [] };
+
+         for (obj of js.obj) {
+            if (obj.num % 3 === 0 && obj.num % 5 === 0) {
+               result.data.push("FizzBuzz");
+            } else if (obj.num % 3 === 0) {
+               result.data.push("Fizz");
+            } else if (obj.num % 5 === 0) {
+               result.data.push("Buzz");
+            } else {
+               result.data.push(obj.num);
+            }
+         }
+         callback(result);
+      }
+
+
+      //TODO：PromiseやCallbackなどを使ってpatternのデータをアクセス
+      //patternが最初に送られる時点でXMLrequestのまま
+
+      fizzBuzz(req.body, (result) => {
+         res.writeHead(200, { 'Content-Type': 'application/json' });
+         res.write();
+         res.close();
+      })
+
+
    }
 });
-server.listen(8080); 
+server.listen(8080);
